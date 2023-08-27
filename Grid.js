334d728856ff1f8cb30279aa141916bbfd4cb89f
StanaAndrei <stadey33@gmail.com>
2023-08-27 10:15:58 +0300

    del js

--- Grid.js
+++
@@ -1,6 +0,0 @@
-export default class Grid {
-    constructor(gridElem) {
-
-    }
-}
-
--- script.js
+++
@@ -1,4 +0,0 @@
-import Grid from "./Grid"
-
-const gameBoard = document.querySelector('#game-board')
-const grid = new Grid(gameBoard);
